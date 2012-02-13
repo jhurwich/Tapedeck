6182f21bbce6bfa671915d43dbfec9d88be3a0eb
Jordan Hurwich <jhurwich@Tardis.(none)>
2012-02-12 19:13:07 -0800

    Template in a better place

--- src/backend/models/cassettes/cassettify-template.js
+++ src/backend/models/cassettes/cassettify-template.js
@@ -16,28 +16,52 @@ Tapedeck.Backend.CassetteManager.CassettifyTemplate = {
     getBrowseList: function(context, callback) { \
       console.log("GETTING BROWSE LIST"); \
       var self = this; \
+      var ourDump = $("#dump").find("#CassetteFromTemplate"); \
+      if (ourDump.length == 0) { \
+        console.log("making new dump"); \
+        ourDump = $("<div id=\'CassetteFromTemplate\'>"); \
+        $(ourDump).appendTo($("#dump")); \
+      } \
  \
-      var parseResponse = function(data, status, xhr) { \
-        $("#dump").html(""); \
-        var responseText = xhr.responseText; \
-        var cleanedText = Tapedeck.Backend.ParserSuite.Util.removeUnwantedTags(responseText); \
-        $("#dump").append(cleanedText); \
-   \
-        Tapedeck.Backend.TrackParser.start($("#dump"), callback); \
+      var ourDumpIsCached = function() { \
+        if ($(ourDump).attr("filled-at") != null) { \
+          var filled = parseInt($(ourDump).attr("filled-at")); \
+          var diff = (new Date()).getTime() - filled; \
+          console.log("diff: " + diff); \
+          return (diff / 1000 / 60 / 60 < 1); \
+        } \
+        else { \
+          return false; \
+        } \
       }; \
  \
-      /* First hit the domain itself, usually the first page */ \
-      $.ajax({ \
-        type: "GET", \
-        url: "http://www." + self.domain, \
-        dataType: "html", \
- \
-        success: parseResponse, \
- \
-        error: function (response) { \
-          console.error("Ajax error retrieving " + self.domain + ""); \
-        }, \
-      }); \
+      if (!ourDumpIsCached()) { \
+        var parseResponse = function(data, status, xhr) { \
+          var responseText = xhr.responseText; \
+          var cleanedText = Tapedeck.Backend.ParserSuite.Util.removeUnwantedTags(responseText); \
+          $(ourDump).append(cleanedText); \
+          $(ourDump).attr("filled-at", (new Date()).getTime()); \
+     \
+          Tapedeck.Backend.TrackParser.start($(ourDump), callback); \
+        }; \
+   \
+        /* First hit the domain itself, usually the first page */ \
+        $.ajax({ \
+          type: "GET", \
+          url: "http://www." + self.domain, \
+          dataType: "html", \
+   \
+          success: parseResponse, \
+   \
+          error: function (response) { \
+            console.error("Ajax error retrieving " + self.domain + ""); \
+          }, \
+        }); \
+      } \
+      else { \
+        /* the dump for this cassette is cached and non-stale */ \
+        Tapedeck.Backend.TrackParser.start($(ourDump), callback); \
+      } \
  \
     }, /* end getBrowseList */ \
   }); \
